da47bb3c4cf22297f94a248df87ce325a96e4cfa
Isoheptane <chomodiso@outlook.com>
2024-11-27 00:03:45 +0800

    Make tungstenite non-blocking

--- src/client.rs
+++ src/client.rs
@@ -20,6 +20,7 @@ pub struct LiveClient {
 #[derive(Debug, Display)]
 pub enum ClientError {
     TungsteniteError(tungstenite::Error),
+    IOError(std::io::Error),
     ConnectionClosed,
     PacketProcessError
 }
@@ -28,6 +29,7 @@ impl error::Error for ClientError {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match &self {
             Self::TungsteniteError(e) => Some(e),
+            Self::IOError(e) => Some(e),
             Self::ConnectionClosed => None,
             Self::PacketProcessError => None
         }
@@ -36,8 +38,20 @@ impl error::Error for ClientError {
 
 impl LiveClient {
     pub fn new(host_url: &str, session: SessionData) -> Result<Self, ClientError> {
+        
         let (mut client, _) = tungstenite::connect(host_url)
             .map_err(|e| ClientError::TungsteniteError(e))?;
+
+        match client.get_mut() {
+            MaybeTlsStream::Plain(stream) => {
+                stream.set_nonblocking(true)
+            },
+            MaybeTlsStream::NativeTls(stream) => {
+                stream.get_mut().set_nonblocking(true)
+            },
+            _ => unimplemented!()
+        }
+        .map_err(|e| ClientError::IOError(e))?;
 
         let certificate = Packet::new_certificate_packet(session.uid, session.room_id, &session.token)
             .map_err(|_| ClientError::PacketProcessError)?
@@ -63,12 +77,17 @@ impl LiveClient {
         let mut messages: Vec<DepackedMessage> = vec![];
         // Read all packets
         loop {
-            if !self.client.can_read() {
-                return Ok(messages);
-            };
             let msg = match self.client.read() {
                 Ok(x) => x,
-                Err(e) => return Err(ClientError::TungsteniteError(e))
+                Err(tungstenite::Error::Io(e)) => {
+                    if e.kind() == std::io::ErrorKind::WouldBlock {
+                        return Ok(messages);
+                    }
+                    return Err(ClientError::TungsteniteError(tungstenite::Error::Io(e)));
+                },
+                Err(e) => {
+                    return Err(ClientError::TungsteniteError(e));
+                }
             };
             if msg.is_close() {
                 self.connected = false;
